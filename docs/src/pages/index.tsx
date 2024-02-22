46a988636d946d320e825a1bb398fe36185445ef
Emmanuel Lobo <76094069+UnschooledGamer@users.noreply.github.com>
2024-02-22 22:16:51 +0530

    Update index.tsx

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/api">
-            Get Started
+            🚀 Get Started
           </Link>
         </div>
       </div>
